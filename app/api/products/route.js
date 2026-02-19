36f45d3a2c80d61f32f24fdf2511db13ca857a86
Jenny C <jjennycao@gmail.com>
2026-02-19 00:43:43 -0500

    Homepage fetching and displaying products

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,3 +1,14 @@
 import { supabase } from "@/lib/supabase"
-
+// fetch products from the database and return them as json
 export async function GET(request) {
+    const { data, error } = await supabase
+    .from('matcha_products')
+    .select()
+    .order('created_at', { ascending: false })
+
+    if (error) {
+        return new Response(JSON.stringify({ error: error.message }), { status: 500 })
+    }
+    return Response.json({products: data})
+}
+
